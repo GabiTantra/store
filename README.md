09f128d87261f76ccb984b9d4055e33a747ef0ff
GabiTantra <tantra.gabriela@escolar.ifrn.edu.br>
2021-12-15 08:32:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ty136y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ty136y)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, VERSION } from '@angular/core';
+import { Produto, __produtos } from '../model/produto';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  produtos: Produto[];
+
+  constructor() {
+    this.produtos = __produtos;
+  }
+}
